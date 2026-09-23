d9feb438c6435a1ec097fae617e4a78a40f142d9
KickDown <oliveiradacruzcaio@gmail.com>
2026-09-23 13:31:57 -0300

    feat: cria a organização básica de páginas do projeto

---
+++ src/app/access/page.tsx
@@ -0,0 +1,5 @@
+export default function Access() {
+    return (
+        <h1>AquaSmart Acesso</h1>
+    )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+    return (
+        <h1>AquaSmart Dashboard</h1>
+    )
+}
